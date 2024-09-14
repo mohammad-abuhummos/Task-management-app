70f7faf013016a0d90c9ab5fcd5ef96f77a39072
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2024-09-14 12:11:43 +0300

    Implement filter group and items

---
+++ src/components/filter/FilterGroup.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import FilterItems from './FilterItems';
+import './FilterGroup.css';
+interface FilterGroupProps {
+    title: string;
+    options: string[];
+    name: string;
+    value?: string | null;
+    onOptionChange: (option: string) => void;
+}
+
+const FilterGroup: React.FC<FilterGroupProps> = ({ title, value, options, name, onOptionChange }) => {
+    const [selectedOption, setSelectedOption] = useState<string | null>(value ?? null);
+
+    const handleOptionChange = (option: string) => {
+        setSelectedOption(option);
+        onOptionChange(option);
+    };
+
+    return (
+        <div className="filter-group">
+            <p>{title}</p>
+            <div className="filter-items">
+                {options.map((option) => (
+                    <FilterItems
+                        key={option}
+                        option={option}
+                        name={name}
+                        checked={selectedOption === option}
+                        onChange={handleOptionChange}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default FilterGroup;
