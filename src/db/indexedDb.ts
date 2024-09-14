85b20ac190965da943d0ad4be0a7d122384d4da8
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2024-09-14 11:10:51 +0300

    implement database CRUD

---
+++ src/db/indexedDb.ts
@@ -0,0 +1,47 @@
+import { openDB } from 'idb';
+import { FilterType, Task } from '../types/types';
+
+const dbPromise = openDB('tasks-db', 1, {
+  upgrade(db) {
+    if (!db.objectStoreNames.contains('tasks')) {
+      db.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
+    }
+  },
+});
+
+export const addTaskToDB = async (task: Task) => {
+  const db = await dbPromise;
+  return db.add('tasks', task);
+};
+
+export const getTasksFromDB = async (filter?:FilterType| undefined ) => {
+  const db = await dbPromise;
+  let tasks = await db.getAll('tasks');
+  if (filter) {
+    if (!!filter.status && filter.status !== 'all') {
+        tasks = tasks.filter((task: Task) => task.status === filter.status);
+      }
+    
+      if (!!filter.category && filter.category !== 'all') {
+        tasks = tasks.filter((task: Task) => !!task.categories && task.categories.includes(filter.category!))
+      }
+  }
+  return tasks;
+};
+
+export const updateTaskInDB = async (task: Task) => {
+  console.log('task', task);
+  try {
+    const db = await dbPromise;
+    return db.put('tasks', task);
+    
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteTaskFromDB = async (id: number) => {
+  const db = await dbPromise;
+  return db.delete('tasks', id);
+};
+
